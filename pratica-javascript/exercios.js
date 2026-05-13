36e7ff2416c63f9c601c3b287bd8faae81b099af
Giovanni <giovanni_ge@hotmail.com>
2026-05-13 16:13:17 -0300

    feat: add

---
+++ pratica-javascript/exercios.js
@@ -0,0 +1,11 @@
+const num1 = 2;
+const num2 = 4;
+const operacao = 'soma';
+
+if (operacao === 'soma') {
+    console.log(num1 + num2)
+} else if (operacao === 'multiplicacao') {
+    console.log(num1 * num2)
+} else {
+    console.log("Operação não identificada")
+}
