d5a98865ffd0c710f9ea4d550c605feb01601278
Giovanni <giovanni_ge@hotmail.com>
2026-08-13 16:38:28 -0300

    adicionando projeto

---
+++ praticando-python/condicional/calculo-pedagio.py
@@ -0,0 +1,8 @@
+distancia  = float(input("Digite a distância percorrida (em km): "))
+
+if distancia <= 100:
+    print("Valor do pedágio: R$ 10,00")
+elif 100 < distancia <= 200:
+    print("Valor do pedágio: R$ 20,00")
+else:
+    print("Valor do pedágio: R$ 30,00")
