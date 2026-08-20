168661262bd3509516acfe125b33af2954d134e9
Giovanni <giovanni_ge@hotmail.com>
2026-08-20 11:33:50 -0300

    feat: add

---
+++ praticando-python/laco-repeticao/contagem-regressiva.py
@@ -0,0 +1,5 @@
+for segundos in range(10, 0, -1):
+    if segundos % 2 == 0:
+        print(f"Faltam apenas {segundos} segundos - Não perca essa oportunidade")
+
+print("Aproveite a promoção agora!")   
